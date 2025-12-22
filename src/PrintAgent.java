37de4221fde617fb7ab03117be8c636cc86d1b84
Ram-potadar <potadarrp2004@gmail.com>
2025-12-22 23:58:29 +0530

    Added print range and no. of copies

--- src/PrintAgent.java
+++ src/PrintAgent.java
@@ -3,6 +3,11 @@ import java.awt.print.PrinterJob;
 
 import javax.print.PrintService;
 import javax.print.PrintServiceLookup;
+import javax.print.attribute.HashPrintRequestAttributeSet;
+import javax.print.attribute.PrintRequestAttributeSet;
+import javax.print.attribute.standard.Copies;
+import javax.print.attribute.standard.PageRanges;
+import javax.print.attribute.standard.Chromaticity;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.printing.PDFPrintable;
@@ -11,24 +16,34 @@ public class PrintAgent {
 
     public static void main(String[] args) {
         try {
-            File pdfFile = new File("pdf/test.pdf");
-
+            File pdfFile = new File("D:/Printer/Codes/agent/pdf/1.pdf");
             PDDocument document = PDDocument.load(pdfFile);
 
             PrintService printer = PrintServiceLookup.lookupDefaultPrintService();
-            if (printer == null) {
-                System.out.println("No printer found");
-                return;
-            }
 
             PrinterJob job = PrinterJob.getPrinterJob();
             job.setPrintService(printer);
             job.setPrintable(new PDFPrintable(document));
 
-            job.print();
+            boolean customPage = true;
+            int startPage = 1;
+            int endPage = 4;
 
+            PrintRequestAttributeSet attributes =
+                    new HashPrintRequestAttributeSet();
+
+            attributes.add(new Copies(1));
+
+            if (customPage) {
+                attributes.add(new PageRanges(startPage, endPage));
+            }
+
+
+
+            job.print(attributes);
             document.close();
-            System.out.println("Print job sent successfully");
+
+            System.out.println("Printed with copies");
 
         } catch (Exception e) {
             e.printStackTrace();
